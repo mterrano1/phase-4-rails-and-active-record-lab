1a1b5d4cbc60513668ce03e860c7d72f751c4597
Christopher Terrano <mterrano1@yahoo.com>
2022-06-02 17:05:14 -0500

    commit tree 2166522210627b3213553cfaf23ad88cc2f2fade

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+fred = Student.create(first_name: 'Fred', last_name: 'Smith', grade: '5')
